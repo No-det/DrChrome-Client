9e7fef2c96066b278c80b8e3d09840b8bf24d6a7
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-12-24 23:42:12 +0530

    Almost done with 404 Page

--- src/Components/404/index.js
+++ src/Components/404/index.js
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
 import "./index.css";
+import image from "../../assets/404.svg";
 
-const NotFound = (props) => {
-  return <div className="nf_main">404</div>;
+const NotFound = () => {
+  return (
+    <div className="nf__main">
+      <div className="nf__left">
+        <div className="nf__top">
+          <blockquote>
+            It is health that is the real wealth, and not pieces of gold and
+            silver.
+          </blockquote>
+          <p className="nf__credit">
+            <i>Mahatma Gandhi</i>
+          </p>
+        </div>
+        <div className="nf__bottom">
+          <Link to="/">Back To Home page</Link>
+        </div>
+      </div>
+      <div className="nf__right">
+        <img src={image} alt="Page Not Found" />
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
