211b3be03ebe52af86472bc0d4a034713a5d1592
Nandan-unni <asnqln@gmail.com>
2020-12-24 21:54:41 +0530

    Responsive

--- src/Components/Index/Designs/index.js
+++ src/Components/Index/Designs/index.js
@@ -1,15 +1,16 @@
 import "./index.css";
 
+
 export default function Shape (props) {
     return (
         <div 
             className="shape"
             style={{
-                background: props.shape === "square" || props.shape === "ellipse" ? "#01D9AD" : "#009BD9",
-                borderRadius: props.shape === "square" ? "20px" : props.shape === "ellipse" ? "0px 65.5px" : "100px"
+                background: props.shape === "circle" ? "#009BD9" : "#01D9AD",
+                borderRadius: props.shape === "square" ? `${window.innerWidth > 600 ? 20 : 12}px` : props.shape === "ellipse" ? `0px ${window.innerWidth > 600 ? 65.5 : 20}px` : "100%"
             }}
         >
-            {/*  */}
+            {/* {console.log(window.innerWidth)} */}
         </div>
     );
 }
