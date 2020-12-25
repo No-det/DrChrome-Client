e5eec5c874dd886ba8b325f43587b42ae93cb8e4
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-12-25 22:26:47 +0530

    Done half UI for the sign up page second part for the patient

--- src/Components/SignUp2/index.js
+++ src/Components/SignUp2/index.js
@@ -2,155 +2,159 @@ import "./index.css";
 import { Form, Select, Button, DatePicker, Input } from "antd";
 const { Option } = Select;
 
-const formItemLayout = {
-  labelCol: {
-    span: 6,
-  },
-  wrapperCol: {
-    span: 14,
-  },
-};
-
 const SignUp2 = () => {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
   };
 
   return (
-    <Form
-      name="validate_other"
-      {...formItemLayout}
-      onFinish={onFinish}
-      //   initialValues={{
-      //     ["input-number"]: 3,
-      //     ["checkbox-group"]: ["A", "B"],
-      //     rate: 3.5,
-      //   }}
-    >
-      <Form.Item
-        name="name"
-        label="Name"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please Enter Your Name",
-          },
-        ]}
-      >
-        <Input placeholder="Enter Your Name" />
-      </Form.Item>
+    <div className="signup2__container">
+      <div className="signup2__left">{/* <h1>Dr.Chrome</h1> */}</div>
+      <div className="signup2__mid">
+        <h3>Create Your Free Account Today</h3>
+      </div>
+      <div className="signup2__right">
+        <Form
+          name="validate_other"
+          onFinish={onFinish}
+          //   initialValues={{
+          //     ["input-number"]: 3,
+          //     ["checkbox-group"]: ["A", "B"],
+          //     rate: 3.5,
+          //   }}
+        >
+          <Form.Item
+            name="name"
+            label="Name"
+            hasFeedback
+            className="form__item"
+            rules={[
+              {
+                required: true,
+                message: "Please Enter Your Name",
+              },
+            ]}
+          >
+            <Input placeholder="Enter Your Name" />
+          </Form.Item>
 
-      <Form.Item
-        name="phone"
-        label="Phone Number"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please Enter Your Phone Number",
-          },
-        ]}
-      >
-        <Input placeholder="Enter Your Phone Number" />
-      </Form.Item>
+          <Form.Item
+            name="phone"
+            label="Phone Number"
+            hasFeedback
+            className="form__item"
+            rules={[
+              {
+                required: true,
+                message: "Please Enter Your Phone Number",
+              },
+            ]}
+          >
+            <Input placeholder="Enter Your Phone Number" />
+          </Form.Item>
 
-      <Form.Item
-        name="city"
-        label="City"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please Enter Your City",
-          },
-        ]}
-      >
-        <Input placeholder="Enter Your City" />
-      </Form.Item>
-      <Form.Item
-        name="state"
-        label="State"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please Enter Your State",
-          },
-        ]}
-      >
-        <Input placeholder="Enter Your State" />
-      </Form.Item>
-      <Form.Item
-        name="zip"
-        label="Pin Code"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please Enter Your Pin Code",
-          },
-        ]}
-      >
-        <Input placeholder="Enter Your Pin Code" />
-      </Form.Item>
+          <Form.Item
+            name="city"
+            label="City"
+            hasFeedback
+            className="form__item"
+            rules={[
+              {
+                required: true,
+                message: "Please Enter Your City",
+              },
+            ]}
+          >
+            <Input placeholder="Enter Your City" />
+          </Form.Item>
+          <Form.Item
+            name="state"
+            label="State"
+            hasFeedback
+            className="form__item"
+            rules={[
+              {
+                required: true,
+                message: "Please Enter Your State",
+              },
+            ]}
+          >
+            <Input placeholder="Enter Your State" />
+          </Form.Item>
+          <Form.Item
+            name="zip"
+            label="Pin Code"
+            hasFeedback
+            className="form__item"
+            rules={[
+              {
+                required: true,
+                message: "Please Enter Your Pin Code",
+              },
+            ]}
+          >
+            <Input placeholder="Enter Your Pin Code" />
+          </Form.Item>
 
-      <Form.Item
-        name="dob"
-        label="Date Of Birth"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please Enter Your Date of Birth",
-          },
-        ]}
-      >
-        <DatePicker />
-      </Form.Item>
+          <Form.Item
+            name="dob"
+            label="Date Of Birth"
+            hasFeedback
+            className="form__item"
+            rules={[
+              {
+                required: true,
+                message: "Please Enter Your Date of Birth",
+              },
+            ]}
+          >
+            <DatePicker />
+          </Form.Item>
 
-      <Form.Item
-        name="gender"
-        label="Gender"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Select Gender",
-          },
-        ]}
-      >
-        <Select placeholder="Please select a gender">
-          <Option value="male">Male</Option>
-          <Option value="female">Female</Option>
-          <Option value="other">Other</Option>
-        </Select>
-      </Form.Item>
+          <Form.Item
+            name="gender"
+            label="Gender"
+            hasFeedback
+            className="form__item"
+            rules={[
+              {
+                required: true,
+                message: "Select Gender",
+              },
+            ]}
+          >
+            <Select placeholder="Please select a gender">
+              <Option value="male">Male</Option>
+              <Option value="female">Female</Option>
+              <Option value="other">Other</Option>
+            </Select>
+          </Form.Item>
 
-      <Form.Item
-        name="blood"
-        label="Blood Group"
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Select Blood Group",
-          },
-        ]}
-      >
-        <Select placeholder="Please select your group">
-          <Option value="o+">O+ve</Option>
-          <Option value="o-">O-ve</Option>
-          <Option value="a+">A+ve</Option>
-          <Option value="a-">A-ve</Option>
-          <Option value="b+">B+ve</Option>
-          <Option value="b-">B-ve</Option>
-          <Option value="ab+">AB+ve</Option>
-          <Option value="ab-">AB-ve</Option>
-        </Select>
-      </Form.Item>
+          <Form.Item
+            name="blood"
+            label="Blood Group"
+            hasFeedback
+            className="form__item"
+            rules={[
+              {
+                required: true,
+                message: "Select Blood Group",
+              },
+            ]}
+          >
+            <Select placeholder="Please select your group">
+              <Option value="o+">O+ve</Option>
+              <Option value="o-">O-ve</Option>
+              <Option value="a+">A+ve</Option>
+              <Option value="a-">A-ve</Option>
+              <Option value="b+">B+ve</Option>
+              <Option value="b-">B-ve</Option>
+              <Option value="ab+">AB+ve</Option>
+              <Option value="ab-">AB-ve</Option>
+            </Select>
+          </Form.Item>
 
-      {/*<Form.Item
+          {/*<Form.Item
         name="upload"
         label="Upload"
         valuePropName="fileList"
@@ -162,17 +166,19 @@ const SignUp2 = () => {
         </Upload>
       </Form.Item> */}
 
-      <Form.Item
-        wrapperCol={{
-          span: 12,
-          offset: 6,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Form.Item
+            wrapperCol={{
+              span: 12,
+              offset: 6,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 };
 
