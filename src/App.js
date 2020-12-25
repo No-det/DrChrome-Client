45cee382bce165be62c25f8eb266ea7edc946d46
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2020-12-25 15:05:47 +0530

    Added ANTD components for sign up 2 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import "./App.css";
 import NavbarContext from "./Contexts/Navbar__Context";
 import Router from "./Router";
+
+import "./App.css";
+import "antd/dist/antd.css";
 
 function App() {
   return (
--- src/Components/SignUp2/index.js
+++ src/Components/SignUp2/index.js
@@ -1,41 +1,178 @@
 import "./index.css";
+import { Form, Select, Button, DatePicker, Input } from "antd";
+const { Option } = Select;
+
+const formItemLayout = {
+  labelCol: {
+    span: 6,
+  },
+  wrapperCol: {
+    span: 14,
+  },
+};
 
 const SignUp2 = () => {
-  return (
-    <div className="signup2__main">
-      <div className="signup2__container">
-        <h3>Sign Up</h3>
-        <form className="signup2__form">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" />
-          <label htmlFor="DOB">Date Of Birth</label>
-          <input type="date" id="DOB" name="DOB" />
-          <label htmlFor="phone">Phone Number</label>
-          <input type="text" id="phone" name="phone" />
-          <label htmlFor="blood">Blood Group</label>
-          <select name="blood" id="blood">
-            <option value="o+">O+ve</option>
-            <option value="o-">O-ve</option>
-            <option value="A+">A+ve</option>
-            <option value="A-">A-ve</option>
-            <option value="B+">B+ve</option>
-            <option value="B-">B-ve</option>
-            <option value="AB+">AB+ve</option>
-            <option value="AB-">AB-ve</option>
-          </select>
-          <label htmlFor="gender">Gender</label>
-          <select name="gender" id="gender">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-          <label htmlFor="address">Address</label>
-          <textarea name="address" id="address" cols="30" rows="10"></textarea>
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
 
-          <button type="submit">Sign Up</button>
-        </form>
-      </div>
-    </div>
+  return (
+    <Form
+      name="validate_other"
+      {...formItemLayout}
+      onFinish={onFinish}
+      //   initialValues={{
+      //     ["input-number"]: 3,
+      //     ["checkbox-group"]: ["A", "B"],
+      //     rate: 3.5,
+      //   }}
+    >
+      <Form.Item
+        name="name"
+        label="Name"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please Enter Your Name",
+          },
+        ]}
+      >
+        <Input placeholder="Enter Your Name" />
+      </Form.Item>
+
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please Enter Your Phone Number",
+          },
+        ]}
+      >
+        <Input placeholder="Enter Your Phone Number" />
+      </Form.Item>
+
+      <Form.Item
+        name="city"
+        label="City"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please Enter Your City",
+          },
+        ]}
+      >
+        <Input placeholder="Enter Your City" />
+      </Form.Item>
+      <Form.Item
+        name="state"
+        label="State"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please Enter Your State",
+          },
+        ]}
+      >
+        <Input placeholder="Enter Your State" />
+      </Form.Item>
+      <Form.Item
+        name="zip"
+        label="Pin Code"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please Enter Your Pin Code",
+          },
+        ]}
+      >
+        <Input placeholder="Enter Your Pin Code" />
+      </Form.Item>
+
+      <Form.Item
+        name="dob"
+        label="Date Of Birth"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please Enter Your Date of Birth",
+          },
+        ]}
+      >
+        <DatePicker />
+      </Form.Item>
+
+      <Form.Item
+        name="gender"
+        label="Gender"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Select Gender",
+          },
+        ]}
+      >
+        <Select placeholder="Please select a gender">
+          <Option value="male">Male</Option>
+          <Option value="female">Female</Option>
+          <Option value="other">Other</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item
+        name="blood"
+        label="Blood Group"
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Select Blood Group",
+          },
+        ]}
+      >
+        <Select placeholder="Please select your group">
+          <Option value="o+">O+ve</Option>
+          <Option value="o-">O-ve</Option>
+          <Option value="a+">A+ve</Option>
+          <Option value="a-">A-ve</Option>
+          <Option value="b+">B+ve</Option>
+          <Option value="b-">B-ve</Option>
+          <Option value="ab+">AB+ve</Option>
+          <Option value="ab-">AB-ve</Option>
+        </Select>
+      </Form.Item>
+
+      {/*<Form.Item
+        name="upload"
+        label="Upload"
+        valuePropName="fileList"
+        getValueFromEvent={normFile}
+        extra="longgggggggggggggggggggggggggggggggggg"
+      >
+        <Upload name="logo" action="/upload.do" listType="picture">
+          <Button icon={<UploadOutlined />}>Click to upload</Button>
+        </Upload>
+      </Form.Item> */}
+
+      <Form.Item
+        wrapperCol={{
+          span: 12,
+          offset: 6,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
