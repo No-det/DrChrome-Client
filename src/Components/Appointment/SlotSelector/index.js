ecda8edbf67e58cafab9787f2fff5eba7db3bd01
Nandan-unni <asnqln@gmail.com>
2021-01-01 15:15:26 +0530

    appoinment page

--- src/Components/Appointment/SlotSelector/index.js
+++ src/Components/Appointment/SlotSelector/index.js
@@ -1,144 +1,117 @@
 import "./index.css";
-import { useState, useEffect } from 'react';
-import { weekday, getWeek } from "./app";
+import { useState } from 'react';
+import { weekday, getWeek, getMonth } from "./app";
 
 
 
 export default function SlotSelector () {
 
-    let date = new Date();
-
-    const [slotTime, setslotTime] = useState(0);
-    const [slotDay, setslotDay] = useState(date.getDate());
-    const [slotMonth, setslotMonth] = useState(date.getMonth() + 1);
-    const [slotYear, setslotYear] = useState(date.getFullYear());
-    const [slotWeek, setslotWeek] = useState(getWeek(date));
-
-    useEffect(() => {
-        // setslotWeek(getWeek(`${slotYear}-${slotMonth}-`));
-        return () => {
-            // 
-        }
-    }, [])
+    const [slotTime, setslotTime] = useState("Not Selected");
+    const [slotDate, setslotDate] = useState(new Date());
+    const [slotWeek, setslotWeek] = useState(getWeek());
 
     return <div className="slotSelector">
         <div className="monthSelector">
-            <select 
-                size="1" 
-                value={slotWeek[3].getMonth() + 1} 
-                onChange={(e) => setslotWeek(getWeek(`${slotWeek[3].getFullYear()}-${e.target.value}-${slotWeek[3].getDate()}`))}
-            >
-                <option value="1">January</option>
-                <option value="2">February</option>
-                <option value="3">March</option>
-                <option value="4">April</option>
-                <option value="5">May</option>
-                <option value="6">June</option>
-                <option value="7">July</option>
-                <option value="8">August</option>
-                <option value="9">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-            </select>
-            <p>{slotWeek[3].getDate()} / {slotWeek[3].getMonth() + 1} / {slotWeek[3].getFullYear()} == {slotTime}</p>
-            <button>Back to Today</button>
+            <div className="monthSlot">{getMonth[slotWeek[3].getMonth()]}</div>
+            <div className="slot-dtls">
+                <p>{slotDate.getDate()} - {slotDate.getMonth() + 1} - {slotDate.getFullYear()}</p>
+                <p>{slotTime}</p>
+            </div>
+            <button onClick={() => setslotDate(new Date())}>Back to Today</button>
         </div>
         <div className="weekSelector">
             <div className="daySelector">
-                <span 
-                    className="material-icons" 
-                    onClick={() => setslotWeek(getWeek(slotWeek[3].setDate(slotWeek[3].getDate() - 6)))}
-                >
-                    keyboard_arrow_left
-                </span>
                 <div className="dayBlocks">
-                    <div className="dayBlock" onClick={() => setslotWeek(getWeek(slotWeek[0]))}>
+                    <div 
+                        className={`dayBlock ${slotWeek[0].getDate() === slotDate.getDate() ? "selected" : "" }`} 
+                        onClick={() => setslotDate(slotWeek[0])}
+                    >
                         <span>{weekday(slotWeek[0].getDay())}</span>
                         <span>{slotWeek[0].getDate()}</span>
                     </div>
-                    <div className="dayBlock" onClick={() => setslotWeek(getWeek(slotWeek[1]))}>
+                    <div 
+                        className={`dayBlock ${slotWeek[1].getDate() === slotDate.getDate() ? "selected" : "" }`} 
+                        onClick={() => setslotDate(slotWeek[1]) 
+                    }>
                         <span>{weekday(slotWeek[1].getDay())}</span>
                         <span>{slotWeek[1].getDate()}</span>
                     </div>
-                    <div className="dayBlock" onClick={() => setslotWeek(getWeek(slotWeek[2]))}>
+                    <div 
+                        className={`dayBlock ${slotWeek[2].getDate() === slotDate.getDate() ? "selected" : "" }`} 
+                        onClick={() => setslotDate(slotWeek[2]) 
+                    }>
                         <span>{weekday(slotWeek[2].getDay())}</span>
                         <span>{slotWeek[2].getDate()}</span>
                     </div>
-                    <div className="dayBlock selected" onClick={() => setslotWeek(getWeek(slotWeek[3]))}>
+                    <div 
+                        className={`dayBlock ${slotWeek[3].getDate() === slotDate.getDate() ? "selected" : "" }`} 
+                        onClick={() => setslotDate(slotWeek[3]) 
+                    }>
                         <span>{weekday(slotWeek[3].getDay())}</span>
                         <span>{slotWeek[3].getDate()}</span>
                     </div>
-                    <div className="dayBlock" onClick={() => setslotWeek(getWeek(slotWeek[4]))}>
+                    <div 
+                        className={`dayBlock ${slotWeek[4].getDate() === slotDate.getDate() ? "selected" : "" }`} 
+                        onClick={() => setslotDate(slotWeek[4]) 
+                    }>
                         <span>{weekday(slotWeek[4].getDay())}</span>
                         <span>{slotWeek[4].getDate()}</span>
                     </div>
-                    <div className="dayBlock" onClick={() => setslotWeek(getWeek(slotWeek[5]))}>
+                    <div 
+                        className={`dayBlock ${slotWeek[5].getDate() === slotDate.getDate() ? "selected" : "" }`} 
+                        onClick={() => setslotDate(slotWeek[5]) 
+                    }>
                         <span>{weekday(slotWeek[5].getDay())}</span>
                         <span>{slotWeek[5].getDate()}</span>
                     </div>
-                    <div className="dayBlock" onClick={() => setslotWeek(getWeek(slotWeek[6]))}>
+                    <div 
+                        className={`dayBlock ${slotWeek[6].getDate() === slotDate.getDate() ? "selected" : "" }`} 
+                        onClick={() => setslotDate(slotWeek[6]) 
+                    }>
                         <span>{weekday(slotWeek[6].getDay())}</span>
                         <span>{slotWeek[6].getDate()}</span>
                     </div>
                 </div>
-                <span 
-                    className="material-icons"
-                    onClick={() => setslotWeek(getWeek(slotWeek[3].setDate(slotWeek[3].getDate() + 6)))}
-                >
-                    keyboard_arrow_right
-                </span>
             </div>
             <div className="timeSelector">
                 <div className="dayTimeBlock">
-                    <p className="timeBlock-available" onClick={() => setslotTime('10:00 AM')}>10:00 AM</p>
-                    <p className="timeBlock-passed">06:00 AM</p>
-                    <p className="timeBlock-available" onClick={() => setslotTime('12:00 AM')}>12:00 AM</p>
-                    <p className="timeBlock-notAvailable">09:00 AM</p>
-                    <p className="timeBlock-available" onClick={() => setslotTime('02:00 PM')}>2:00 PM</p>
+                    <p 
+                        className={(slotDate.getDate() === slotWeek[0].getDate() && slotTime === "10:00 AM") ? "timeBlock-selected" : "timeBlock-available"} 
+                        onClick={() => {setslotTime('10:00 AM'); setslotDate(slotWeek[0]); } }
+                    >
+                            10:00 AM
+                    </p>
+                    <p 
+                        className={(slotDate.getDate() === slotWeek[0].getDate() && slotTime === "11:00 AM") ? "timeBlock-selected" : "timeBlock-available"} 
+                        onClick={() => {setslotTime('11:00 AM'); setslotDate(slotWeek[0]); } }
+                    >
+                            11:00 AM
+                    </p>
+                    <p 
+                        className="timeBlock-notAvailable"
+                        onClick={() => alert("Time Slot not Available")}
+                    >
+                            12:00 AM
+                    </p>
+                    <p 
+                        className={(slotDate.getDate() === slotWeek[0].getDate() && slotTime === "01:00 PM") ? "timeBlock-selected" : "timeBlock-available"} 
+                        onClick={() => {setslotTime('01:00 PM'); setslotDate(slotWeek[0]); } }
+                    >
+                            01:00 PM
+                    </p>
+                    <p 
+                        className="timeBlock-notAvailable"
+                        onClick={() => alert("Time Slot not Available")}
+                    >
+                            02:00 AM
+                    </p>
                 </div>
-                <div className="dayTimeBlock">
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-passed">06:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-notAvailable">09:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                </div>
-                <div className="dayTimeBlock">
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-passed">06:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-notAvailable">09:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                </div>
-                <div className="dayTimeBlock">
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-passed">06:00 AM</p>
-                    <p className="timeBlock-available">11:00 AM</p>
-                    <p className="timeBlock-notAvailable">09:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                </div>
-                <div className="dayTimeBlock">
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-passed">06:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-notAvailable">09:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                </div>
-                <div className="dayTimeBlock">
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-passed">06:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-notAvailable">09:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                </div>
-                <div className="dayTimeBlock">
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-passed">06:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                    <p className="timeBlock-notAvailable">09:00 AM</p>
-                    <p className="timeBlock-available">10:00 AM</p>
-                </div>
+                <div className="dayTimeBlock"><p className="timeBlock-available">--</p></div>
+                <div className="dayTimeBlock"><p className="timeBlock-available">--</p></div>
+                <div className="dayTimeBlock"><p className="timeBlock-available">--</p></div>
+                <div className="dayTimeBlock"><p className="timeBlock-available">--</p></div>
+                <div className="dayTimeBlock"><p className="timeBlock-available">--</p></div>
+                <div className="dayTimeBlock"><p className="timeBlock-available">--</p></div>
             </div>
         </div>
     </div>
