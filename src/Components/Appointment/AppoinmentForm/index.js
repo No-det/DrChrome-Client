3d9b406977f7da1f9a2c592b90df828680b5df16
Ajal333 <ajapoos1999@gmail.com>
2021-01-01 13:14:55 +0530

    updates to p tag

--- src/Components/Appointment/AppoinmentForm/index.js
+++ src/Components/Appointment/AppoinmentForm/index.js
@@ -1,18 +1,98 @@
+import { Button, Form, Input, Upload } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 import "./index.css";
+import { useState } from "react";
 
-export default function AppointmentForm () {
-    return (
-        <div className="apt-form">
-            <h3>Appointment Form</h3>
+export default function AppointmentForm() {
+  const [uploading, setUploading] = useState(false);
+  // const [redirect, setRedirect] = useState(false);
+  // const [error, setError] = useState();
+
+  const onFinish = async (values) => {
+    console.log(values);
+  };
+
+  const normFile = (e) => {
+    console.log("Upload event:", e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  };
+
+  return (
+    <div className="apt-form">
+      {/* <h3>Appointment Form</h3>
             <label>What is the reason for your appointment ?</label><br />
             <textarea rows="5"></textarea><br /><br />
             <label>Symptoms (if any)</label><br />
             <textarea rows="5"></textarea><br /><br />
             <label>Attach Documents (if any)</label><br />
             <input type="file" /><br /><br />
-            <button>Make Appointment</button>
-        </div>
-    );
+            <button>Make Appointment</button> */}
+      <Form
+        name="validate_other"
+        onFinish={onFinish}
+        requiredMark={false}
+        // initialValues={{
+        //   name: user.firstName,
+        // }}
+      >
+        <Form.Item
+          name="reason"
+          //   label="Reason for Appointment"
+          hasFeedback
+          className="form__item"
+          rules={[
+            {
+              required: true,
+              message: "Please enter the reason for your appointment: ",
+            },
+          ]}
+        >
+          <Input.TextArea placeholder="Enter Your Reason" allowClear rows={5} />
+        </Form.Item>
+        <Form.Item
+          name="Symptoms"
+          //   label="Symptoms"
+          hasFeedback
+          className="form__item"
+          rules={[
+            {
+              message: "Please Enter Your Symptoms",
+            },
+          ]}
+        >
+          <Input.TextArea
+            placeholder="Enter Your Symptoms"
+            allowClear
+            rows={5}
+          />
+        </Form.Item>
+        <Form.Item
+          name="upload"
+          label="Upload any documents: "
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+        >
+          <Upload name="logo" action="/upload.do" listType="picture">
+            <Button icon={<UploadOutlined />}>Click to upload</Button>
+          </Upload>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            span: 12,
+            offset: 6,
+          }}
+        >
+          <Button type="primary" htmlType="submit" loading={uploading}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 // 414227024002-7msf4kdntm638jgr2uccptmrb3s2kc23.apps.googleusercontent.com
--- src/Components/PatientProfile/index.js
+++ src/Components/PatientProfile/index.js
@@ -1,11 +1,15 @@
 import "./index.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import AppointmentCard from "./AppointmentCard";
 import FileCard from "./FileCard";
+import { AuthContext } from "../../Contexts/Auth__Context";
 
 const PatientProfile = () => {
+  const { user } = useContext(AuthContext);
   const [nav, setNav] = useState(0);
+
+  console.log(user);
 
   const changeNav = (id) => {
     setNav(id);
@@ -14,129 +18,131 @@ const PatientProfile = () => {
   return (
     <div className="patientprofile__main">
       <h3>Patient Profile</h3>
-      <div className="patientprofile__container">
-        <div className="patientprofile__left">
-          <div className="patientprofile__top">
-            <div className="patientprofile__briefdetails">
-              <div className="patientprofile__image">
-                <img
-                  src="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"
-                  alt="DP"
-                />
+      {user && (
+        <div className="patientprofile__container">
+          <div className="patientprofile__left">
+            <div className="patientprofile__top">
+              <div className="patientprofile__briefdetails">
+                <div className="patientprofile__image">
+                  <img
+                    src="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"
+                    alt="DP"
+                  />
+                </div>
+                <div className="patientprofile__name">
+                  <h4>{user.name}</h4>
+                  <p>ajal@ajal.tech</p>
+                </div>
+                <div className="patientprofile__appointments">
+                  <h4>
+                    Appointments: <span>2</span>
+                  </h4>
+                </div>
+                <Link className="patientprofile__editprofile" to="/signup2">
+                  Edit Profile
+                </Link>
               </div>
-              <div className="patientprofile__name">
-                <h4>Frank Morris</h4>
-                <p>frankmorris@nodet.com</p>
+              <div className="patientprofile__details">
+                <div className="patientprofile__detail">
+                  <h5>Gender</h5>
+                  <p>{user.gender}</p>
+                </div>
+                <div className="patientprofile__detail">
+                  <h5>Birthday</h5>
+                  <p>03/12/1999</p>
+                </div>
+                <div className="patientprofile__detail">
+                  <h5>Phone Number</h5>
+                  <p>{user.phone}</p>
+                </div>
+                <div className="patientprofile__detail">
+                  <h5>Street Address</h5>
+                  <p>JK, San Tahome-45</p>
+                </div>
+                <div className="patientprofile__detail">
+                  <h5>City</h5>
+                  <p>San Deigo</p>
+                </div>
+                <div className="patientprofile__detail">
+                  <h5>Zip Code</h5>
+                  <p>888888</p>
+                </div>
+                <div className="patientprofile__detail">
+                  <h5>Registered Date</h5>
+                  <p>23 December, 2020</p>
+                </div>
+                <div className="patientprofile__detail">
+                  <h5>Last Updated</h5>
+                  <p>24 December, 2020</p>
+                </div>
+                <div className="patientprofile__detail">
+                  <h5>Blood Group</h5>
+                  <p>{user.blood}</p>
+                </div>
               </div>
-              <div className="patientprofile__appointments">
-                <h4>
-                  Appointments: <span>2</span>
-                </h4>
-              </div>
-              <Link className="patientprofile__editprofile" to="/">
-                Edit Profile
-              </Link>
             </div>
-            <div className="patientprofile__details">
-              <div className="patientprofile__detail">
-                <h5>Gender</h5>
-                <p>Male</p>
+            <div className="patientprofile__bottom">
+              <div className="patientprofile__nav">
+                <span
+                  onClick={() => changeNav(0)}
+                  className={nav === 0 ? "pp__active" : null}
+                >
+                  Previous Appointment
+                </span>
+                <span
+                  onClick={() => changeNav(1)}
+                  className={nav === 1 ? "pp__active" : null}
+                >
+                  Upcoming Appointment
+                </span>
               </div>
-              <div className="patientprofile__detail">
-                <h5>Birthday</h5>
-                <p>3rd December 1999</p>
-              </div>
-              <div className="patientprofile__detail">
-                <h5>Phone Number</h5>
-                <p>(888) - 8888 888</p>
-              </div>
-              <div className="patientprofile__detail">
-                <h5>Street Address</h5>
-                <p>JK, San Tahome-45</p>
-              </div>
-              <div className="patientprofile__detail">
-                <h5>City</h5>
-                <p>San Deigo</p>
-              </div>
-              <div className="patientprofile__detail">
-                <h5>Zip Code</h5>
-                <p>888888</p>
-              </div>
-              <div className="patientprofile__detail">
-                <h5>Registered Date</h5>
-                <p>23 December, 2020</p>
-              </div>
-              <div className="patientprofile__detail">
-                <h5>Last Updated</h5>
-                <p>24 December, 2020</p>
-              </div>
-              <div className="patientprofile__detail">
-                <h5>Blood Group</h5>
-                <p>A +ve</p>
+              <div className="patientprofile__appointmentsdetails">
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
+                <AppointmentCard />
               </div>
             </div>
           </div>
-          <div className="patientprofile__bottom">
-            <div className="patientprofile__nav">
-              <span
-                onClick={() => changeNav(0)}
-                className={nav === 0 ? "pp__active" : null}
-              >
-                Previous Appointment
-              </span>
-              <span
-                onClick={() => changeNav(1)}
-                className={nav === 1 ? "pp__active" : null}
-              >
-                Upcoming Appointment
-              </span>
-            </div>
-            <div className="patientprofile__appointmentsdetails">
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
-              <AppointmentCard />
+          <div className="patientprofile__right">
+            <h5>Files/Document</h5>
+            <div className="patientprofile__files">
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
+              <FileCard />
             </div>
           </div>
         </div>
-        <div className="patientprofile__right">
-          <h5>Files/Document</h5>
-          <div className="patientprofile__files">
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-            <FileCard />
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
