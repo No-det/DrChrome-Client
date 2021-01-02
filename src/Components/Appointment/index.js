ac965f48cf2527b66f7ff34f0ea7f0fb1b6c00c1
Ajal333 <ajapoos1999@gmail.com>
2021-01-02 23:19:15 +0530

    ente kayyil ulla changes ellam push cheythittind, eni enthokke venam enn nokitt thodangam asap

--- src/Components/Appointment/index.js
+++ src/Components/Appointment/index.js
@@ -1,52 +1,55 @@
 import "./index.css";
 // import Slot from './Slot';
-import AppoinmentForm from './AppoinmentForm';
-import SlotSelector from './SlotSelector';
+import AppoinmentForm from "./AppoinmentForm";
+import SlotSelector from "./SlotSelector";
 
 const Appointment = () => {
-    return (
-        <div className="apt">
-            <div className="apt-docContainer">
-                <div className="apt-docCard">
-                    <div className="apt-docCard-id">
-                        <img
-                            src="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"
-                            alt="Dr. Smith"
-                        />
-                        <div className="apt-docCard-id-txt">
-                            <h3>Dr. Unni Chekavar</h3>
-                            <h4>Unni's Hospital, New York</h4>
-                            <p>1221, Baker Street, Santa Claus, <br /> New York, New York</p>
-                        </div>
-                    </div>
-                    <div className="apt-docCard-dtls">
-                        <div className="apt-docCard-dtl">
-                            <p>Specialization</p>
-                            <p>Paediatrician</p>
-                        </div>
-                        <div className="apt-docCard-dtl">
-                            <p>Pricing</p>
-                            <p>$60 / hr </p>
-                        </div>
-                        <div className="apt-docCard-dtl">
-                            <p>Total Appoinments</p>
-                            <p>74</p>
-                        </div>
-                        <div className="apt-docCard-dtl">
-                            <p>Years of Experience</p>
-                            <p>5 Yrs</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="apt-slotSelector">
-                    <SlotSelector />
-                </div>
+  return (
+    <div className="apt">
+      <div className="apt-docContainer">
+        <div className="apt-docCard">
+          <div className="apt-docCard-id">
+            <img
+              src="https://images.vexels.com/media/users/3/145908/preview2/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker.jpg"
+              alt="Dr. Smith"
+            />
+            <div className="apt-docCard-id-txt">
+              <h3 style={{ marginBottom: 15 }}>Dr. Unni Chekavar</h3>
+              <h4 style={{ marginBottom: 10 }}>Unni's Hospital, New York</h4>
+              <p>
+                1221, Baker Street, Santa Claus, <br /> <br /> New York, New
+                York
+              </p>
             </div>
-            <div className="apt-formContainer">
-                <AppoinmentForm />
+          </div>
+          <div className="apt-docCard-dtls">
+            <div className="apt-docCard-dtl">
+              <p>Specialization</p>
+              <p>Paediatrician</p>
             </div>
+            <div className="apt-docCard-dtl">
+              <p>Pricing</p>
+              <p>$60 / hr </p>
+            </div>
+            <div className="apt-docCard-dtl">
+              <p>Total Appoinments</p>
+              <p>74</p>
+            </div>
+            <div className="apt-docCard-dtl">
+              <p>Years of Experience</p>
+              <p>5 Yrs</p>
+            </div>
+          </div>
         </div>
-    );
+        <div className="apt-slotSelector">
+          <SlotSelector />
+        </div>
+      </div>
+      <div className="apt-formContainer">
+        <AppoinmentForm />
+      </div>
+    </div>
+  );
 };
 
 export default Appointment;
